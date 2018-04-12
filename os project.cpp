8e2d4ae8fa8cb91875db4dcfc95929846dcc9c7b
AnushaReddy1999 <38324329+Anusha1999@users.noreply.github.com>
2018-04-12 21:10:04 +0530

    Update os project.cpp

--- os project.cpp
+++ os project.cpp
@@ -40,34 +40,3 @@ void Sorting()
       }
 }
 
-int main()
-{
-      int i, time = 0, bt = 0;
-      printf("\nEnter Processes:\t");
-      scanf("%d", &process_list);
-      for(i = 0; i < process_list; i++)
-      {
-            queue[i].process_name = i+1;
-            printf("\nEnter Details For Process[%d]:\n", queue[i].process_name);
-            queue[i].finish = 0;
-            printf("Arrival Time:\t");
-            scanf("%d", &queue[i].at );
-            printf("Burst Time:\t");
-            scanf("%d", &queue[i].bt); 
-            printf("type:\t");
-            scanf("%d", &queue[i].type);
-            bt = bt + queue[i].bt;
-      }
-      Sorting();
-      printf("\n type \n200   -   student\n400  -  teacher\n");
-      printf("\nProcess\tArrival\tBurst_Time\ttype\tWaiting Time\n");
-      for(time = queue[0].at,i=0; time < bt;i++)
-      {
-            time = time + queue[i].bt;
-            queue[i].ct = time;
-            queue[i].wai_t = queue[i].ct - queue[i].at - queue[i].bt;
-            queue[i].turnaround_time = queue[i].ct - queue[i].at;
-            queue[i].finish = 1;
-            printf("%d\t%d\t%d\t\t%d\t%d\n", queue[i].process_name, queue[i].at, queue[i].bt,     queue[i].type, queue[i].wai_t);
-      }
-    }
